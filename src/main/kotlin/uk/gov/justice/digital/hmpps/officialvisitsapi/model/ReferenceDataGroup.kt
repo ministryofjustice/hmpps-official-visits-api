3356f46da9f284d44c8028693139f707b7e3ef82
Tim Harrison <tim.harrison@digital.justice.gov.uk>
2025-10-22 17:10:21 +0100

    OV-101: Alphabetic ordering in enum class.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/officialvisitsapi/model/ReferenceDataGroup.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/officialvisitsapi/model/ReferenceDataGroup.kt
@@ -4,13 +4,13 @@ import io.swagger.v3.oas.annotations.media.Schema
 
 @Schema(enumAsRef = true)
 enum class ReferenceDataGroup(val displayName: String, val isDocumented: Boolean) {
-  VIS_STS("visit status code", true),
-  VIS_COMPLETE("visit completion code", true),
-  VIS_TYPE_CODE("visit type", true),
-  VIS_LOC_TYPE("visit location type", true),
-  SEARCH_LEVEL("visitor search type", true),
+  DAY("day of the week", true),
   CONTACTS("contact type", true),
   EQUIP_CATEGORY("equipment category", true),
-  DAY("day of the week", true),
+  SEARCH_LEVEL("visitor search type", true),
   TEST_TYPE("test type", false),
+  VIS_COMPLETE("visit completion code", true),
+  VIS_LOC_TYPE("visit location type", true),
+  VIS_STS("visit status code", true),
+  VIS_TYPE_CODE("visit type", true),
 }
