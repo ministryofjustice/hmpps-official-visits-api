930c00a40b0bd4303b456b0002cdffb01c35e6df
sandeep.khajuria <sankhajuria@gmail.com>
2025-11-14 16:16:53 +0000

    OV-159 : Get Approved contacts for prisoner base on type and prisoner code .

--- src/main/kotlin/uk/gov/justice/digital/hmpps/officialvisitsapi/client/personalrelationships/PersonalRelationshipsApiClient.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/officialvisitsapi/client/personalrelationships/PersonalRelationshipsApiClient.kt
@@ -2,7 +2,6 @@ package uk.gov.justice.digital.hmpps.officialvisitsapi.client.personalrelationsh
 
 import org.slf4j.LoggerFactory
 import org.springframework.core.ParameterizedTypeReference
-import org.springframework.data.web.PagedModel
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.WebClientResponseException
