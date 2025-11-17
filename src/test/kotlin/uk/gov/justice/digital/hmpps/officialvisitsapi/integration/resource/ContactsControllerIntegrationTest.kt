5fa2749f9b7e2b953bd0e2dcf49bf66c29fac8cc
sandeep.khajuria <sankhajuria@gmail.com>
2025-11-17 15:38:38 +0000

    OV-159 : Updated code based on review comments

--- src/test/kotlin/uk/gov/justice/digital/hmpps/officialvisitsapi/integration/resource/ContactsControllerIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/officialvisitsapi/integration/resource/ContactsControllerIntegrationTest.kt
@@ -3,29 +3,29 @@ package uk.gov.justice.digital.hmpps.officialvisitsapi.integration.resource
 import org.junit.jupiter.api.Test
 import org.springframework.http.MediaType
 import org.springframework.test.web.reactive.server.WebTestClient
-import uk.gov.justice.digital.hmpps.officialvisitsapi.client.personalrelationship.model.PrisonerContactSummary
 import uk.gov.justice.digital.hmpps.officialvisitsapi.helper.MOORLAND_PRISON_USER
 import uk.gov.justice.digital.hmpps.officialvisitsapi.helper.isEqualTo
 import uk.gov.justice.digital.hmpps.officialvisitsapi.integration.IntegrationTestBase
+import uk.gov.justice.digital.hmpps.officialvisitsapi.model.response.ApprovedContact
 
 class ContactsControllerIntegrationTest : IntegrationTestBase() {
 
   @Test
   fun `should get all approved contacts for official visits with the valid prisoner number`() {
     personalRelationshipsApi().stubApprovedContacts("A1234BC")
-    val response = webTestClient.approvedContents(prisonerNumber = "A1234BC", type = "O")
+    val response = webTestClient.approvedContents(prisonerNumber = "A1234BC", relationshipType = "O")
     response.size isEqualTo 1
     response.single().prisonerNumber isEqualTo "A1234BC"
   }
 
-  private fun WebTestClient.approvedContents(prisonerNumber: String, type: String) = this
+  private fun WebTestClient.approvedContents(prisonerNumber: String, relationshipType: String) = this
     .get()
-    .uri("/prisoner/$prisonerNumber/contact-relationships?type=$type")
+    .uri("/prisoner/$prisonerNumber/contact-relationships?type=$relationshipType")
     .accept(MediaType.APPLICATION_JSON)
     .headers(setAuthorisation(username = MOORLAND_PRISON_USER.username, roles = listOf("ROLE_CONTACTS__R")))
     .exchange()
     .expectStatus().isOk
     .expectHeader().contentType(MediaType.APPLICATION_JSON)
-    .expectBodyList(PrisonerContactSummary::class.java)
+    .expectBodyList(ApprovedContact::class.java)
     .returnResult().responseBody!!
 }
