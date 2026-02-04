952ad1a7d76e7dc49689db9804a5f46f3e31b138
Tim Harrison <tim.harrison@digital.justice.gov.uk>
2026-02-04 09:00:53 +0000

    OV-299: Service tests for the sync create of an official visit

--- src/test/kotlin/uk/gov/justice/digital/hmpps/officialvisitsapi/service/sync/SyncOfficialVisitServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/officialvisitsapi/service/sync/SyncOfficialVisitServiceTest.kt
@@ -8,20 +8,33 @@ import org.junit.jupiter.api.assertThrows
 import org.mockito.Mockito.mock
 import org.mockito.kotlin.reset
 import org.mockito.kotlin.verify
+import org.mockito.kotlin.verifyNoInteractions
 import org.mockito.kotlin.whenever
 import uk.gov.justice.digital.hmpps.officialvisitsapi.entity.OfficialVisitEntity
+import uk.gov.justice.digital.hmpps.officialvisitsapi.entity.PrisonVisitSlotEntity
+import uk.gov.justice.digital.hmpps.officialvisitsapi.entity.PrisonerVisitedEntity
+import uk.gov.justice.digital.hmpps.officialvisitsapi.helper.MOORLAND
+import uk.gov.justice.digital.hmpps.officialvisitsapi.helper.MOORLAND_PRISONER
 import uk.gov.justice.digital.hmpps.officialvisitsapi.helper.createAPrisonerVisitedEntity
 import uk.gov.justice.digital.hmpps.officialvisitsapi.helper.createAVisitEntity
+import uk.gov.justice.digital.hmpps.officialvisitsapi.helper.tomorrow
+import uk.gov.justice.digital.hmpps.officialvisitsapi.model.request.sync.SyncCreateOfficialVisitRequest
 import uk.gov.justice.digital.hmpps.officialvisitsapi.model.response.sync.SyncOfficialVisit
 import uk.gov.justice.digital.hmpps.officialvisitsapi.repository.OfficialVisitRepository
 import uk.gov.justice.digital.hmpps.officialvisitsapi.repository.PrisonVisitSlotRepository
 import uk.gov.justice.digital.hmpps.officialvisitsapi.repository.PrisonerVisitedRepository
+import java.time.LocalDateTime
+import java.time.LocalTime
 import java.util.Optional
+import java.util.UUID
 
 class SyncOfficialVisitServiceTest {
   private val officialVisitRepository: OfficialVisitRepository = mock()
   private val prisonerVisitedRepository: PrisonerVisitedRepository = mock()
   private val prisonVisitSlotRepository: PrisonVisitSlotRepository = mock()
+
+  private val createdTime = LocalDateTime.now().minusDays(2)
+  private val updatedTime = LocalDateTime.now().minusDays(1)
 
   private val syncOfficialVisitService = SyncOfficialVisitService(
     officialVisitRepository,
@@ -77,15 +90,97 @@ class SyncOfficialVisitServiceTest {
 
   @Test
   fun `should create an official visit`() {
+    val visitSlotEntity = prisonVisitSlotEntity(1L)
+    val request = createOfficialVisitRequest(1L)
+    val officialVisitEntity = OfficialVisitEntity.synchronised(visitSlotEntity, request)
+    val prisonerVisitedEntity = PrisonerVisitedEntity(
+      officialVisit = officialVisitEntity,
+      prisonerNumber = officialVisitEntity.prisonerNumber,
+      createdBy = officialVisitEntity.createdBy,
+      createdTime = officialVisitEntity.createdTime,
+    )
+
+    whenever(prisonVisitSlotRepository.findById(1L)).thenReturn(Optional.of(visitSlotEntity))
+    whenever(officialVisitRepository.saveAndFlush(officialVisitEntity)).thenReturn(officialVisitEntity)
+    whenever(prisonerVisitedRepository.saveAndFlush(prisonerVisitedEntity)).thenReturn(prisonerVisitedEntity)
+
+    val result = syncOfficialVisitService.createOfficialVisit(request)
+
+    with(result) {
+      assertThat(this.prisonCode).isEqualTo(request.prisonCode)
+      assertThat(this.prisonerNumber).isEqualTo(request.prisonerNumber)
+      assertThat(this.visitDate).isEqualTo(request.visitDate)
+      assertThat(this.startTime).isEqualTo(request.startTime)
+      assertThat(this.endTime).isEqualTo(request.endTime)
+      assertThat(this.dpsLocationId).isEqualTo(request.dpsLocationId)
+    }
+
+    verify(prisonVisitSlotRepository).findById(1L)
+    verify(officialVisitRepository).saveAndFlush(officialVisitEntity)
+    verify(prisonerVisitedRepository).saveAndFlush(prisonerVisitedEntity)
   }
 
   @Test
   fun `should fail to create an official visit - references a visit slot that does not exist`() {
+    val request = createOfficialVisitRequest(1L)
+
+    whenever(prisonVisitSlotRepository.findById(1L)).thenReturn(Optional.empty())
+
+    assertThrows<RuntimeException> {
+      syncOfficialVisitService.createOfficialVisit(request)
+    }
+
+    verify(prisonVisitSlotRepository).findById(1L)
+    verifyNoInteractions(officialVisitRepository, prisonerVisitedRepository)
   }
 
   @Test
-  fun `should fail to create an official visit - missing content for required fields`() {
+  fun `should fail to create an official visit - exception on insert`() {
+    val visitSlotEntity = prisonVisitSlotEntity(1L)
+    val request = createOfficialVisitRequest(1L)
+    val officialVisitEntity = OfficialVisitEntity.synchronised(visitSlotEntity, request)
+    val prisonerVisitedEntity = PrisonerVisitedEntity(
+      officialVisit = officialVisitEntity,
+      prisonerNumber = officialVisitEntity.prisonerNumber,
+      createdBy = officialVisitEntity.createdBy,
+      createdTime = officialVisitEntity.createdTime,
+    )
+
+    whenever(prisonVisitSlotRepository.findById(1L)).thenReturn(Optional.of(visitSlotEntity))
+    whenever(officialVisitRepository.saveAndFlush(officialVisitEntity)).thenReturn(officialVisitEntity)
+    whenever(prisonerVisitedRepository.saveAndFlush(prisonerVisitedEntity)).thenThrow(RuntimeException("bang!"))
+
+    assertThrows<RuntimeException> {
+      syncOfficialVisitService.createOfficialVisit(request)
+    }
+
+    verify(prisonVisitSlotRepository).findById(1L)
+    verify(officialVisitRepository).saveAndFlush(officialVisitEntity)
+    verify(prisonerVisitedRepository).saveAndFlush(prisonerVisitedEntity)
   }
+
+  private fun createOfficialVisitRequest(visitSlotId: Long) = SyncCreateOfficialVisitRequest(
+    offenderVisitId = 1L,
+    prisonVisitSlotId = visitSlotId,
+    prisonCode = MOORLAND,
+    offenderBookId = 1L,
+    prisonerNumber = MOORLAND_PRISONER.number,
+    visitDate = tomorrow(),
+    startTime = LocalTime.parse("09:00"),
+    endTime = LocalTime.parse("10:00"),
+    dpsLocationId = UUID.fromString("9485cf4a-750b-4d74-b594-59bacbcda247"),
+    createDateTime = createdTime,
+    createUsername = "Bob",
+  )
+
+  private fun prisonVisitSlotEntity(prisonVisitSlotId: Long = 1L) = PrisonVisitSlotEntity(
+    prisonVisitSlotId = prisonVisitSlotId,
+    prisonTimeSlotId = 1L,
+    dpsLocationId = UUID.fromString("9485cf4a-750b-4d74-b594-59bacbcda247"),
+    maxAdults = 10,
+    createdBy = "Test",
+    createdTime = createdTime,
+  )
 
   private fun OfficialVisitEntity.assertWithResponse(model: SyncOfficialVisit) {
     assertThat(officialVisitId).isEqualTo(model.officialVisitId)
