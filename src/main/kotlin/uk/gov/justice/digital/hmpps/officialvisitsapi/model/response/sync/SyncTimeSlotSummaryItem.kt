870e3cd3fed6b5262b51bdcb6c603b999a314858
sankhajuria <sankhajuria@gmail.com>
2026-02-04 14:16:15 +0000

    OV-305 : Reconciliation endpoint for time-slots and visit slots for prison code (#88)
    * OV-305 : Reconciliation endpoint for time-slots and visit slots for one prison
    
    * OV-305 : Reconciliation endpoint for time-slots and visit slots for one prison
    
    * OV-305 : Reconciliation endpoint for time-slots and visit slots for one prison
    
    * OV-305 : Reconciliation endpoint for time-slots and visit slots for one prison
    
    * OV-305 : Reconciliation endpoint for time-slots and visit slots for one prison
    
    * OV-305 : Reconciliation endpoint for time-slots and visit slots for one prison
    
    * OV-305 : Reconciliation endpoint for time-slots and visit slots for one prison
    
    * Revert visit slot integration test as already covered in time slots
    
    * Moved code reconciliation  service and review comments incorporated
    
    * Test case updated

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/officialvisitsapi/model/response/sync/SyncTimeSlotSummaryItem.kt
@@ -0,0 +1,11 @@
+package uk.gov.justice.digital.hmpps.officialvisitsapi.model.response.sync
+
+import io.swagger.v3.oas.annotations.media.Schema
+
+data class SyncTimeSlotSummaryItem(
+  @Schema(description = "Time Slot")
+  val timeSlot: SyncTimeSlot,
+
+  @Schema(description = "List of visit slots associated with time slot")
+  val visitSlots: List<SyncVisitSlot>,
+)
--- src/test/kotlin/uk/gov/justice/digital/hmpps/officialvisitsapi/integration/resource/sync/SyncVisitSlotIntegrationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/officialvisitsapi/integration/resource/sync/SyncVisitSlotIntegrationTest.kt
@@ -4,7 +4,6 @@ import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.within
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.test.web.reactive.server.WebTestClient
@@ -25,7 +24,6 @@ import uk.gov.justice.digital.hmpps.officialvisitsapi.model.request.sync.SyncCre
 import uk.gov.justice.digital.hmpps.officialvisitsapi.model.request.sync.SyncUpdateVisitSlotRequest
 import uk.gov.justice.digital.hmpps.officialvisitsapi.model.response.CreateOfficialVisitResponse
 import uk.gov.justice.digital.hmpps.officialvisitsapi.model.response.sync.SyncVisitSlot
-import uk.gov.justice.digital.hmpps.officialvisitsapi.repository.PrisonVisitSlotRepository
 import uk.gov.justice.digital.hmpps.officialvisitsapi.service.PrisonUser
 import uk.gov.justice.digital.hmpps.officialvisitsapi.service.events.outbound.OutboundEvent
 import uk.gov.justice.digital.hmpps.officialvisitsapi.service.events.outbound.Source
@@ -37,8 +35,6 @@ import java.time.temporal.ChronoUnit
 import java.util.UUID
 
 class SyncVisitSlotIntegrationTest : IntegrationTestBase() {
-  @Autowired
-  lateinit var prisonVisitSlotRepository: PrisonVisitSlotRepository
   private var savedPrisonVisitSlotId = 0L
 
   private val createdTime = LocalDateTime.now().minusDays(2)
