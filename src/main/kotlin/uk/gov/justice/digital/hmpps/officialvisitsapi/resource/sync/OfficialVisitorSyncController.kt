88764769258daaffed4cd8338c2612ae169ba34a
Tim Harrison <tim.harrison@digital.justice.gov.uk>
2026-02-25 14:17:42 +0000

    OV-389: Refactoring sync for visitor update. Split controllers for sync visit and visitor.

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/officialvisitsapi/resource/sync/OfficialVisitorSyncController.kt
@@ -0,0 +1,137 @@
+package uk.gov.justice.digital.hmpps.officialvisitsapi.resource.sync
+
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.validation.Valid
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseBody
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.officialvisitsapi.facade.sync.SyncFacade
+import uk.gov.justice.digital.hmpps.officialvisitsapi.model.request.sync.SyncCreateOfficialVisitorRequest
+import uk.gov.justice.digital.hmpps.officialvisitsapi.model.request.sync.SyncUpdateOfficialVisitorRequest
+import uk.gov.justice.digital.hmpps.officialvisitsapi.model.response.sync.SyncOfficialVisitor
+import uk.gov.justice.digital.hmpps.officialvisitsapi.resource.AuthApiResponses
+import uk.gov.justice.hmpps.kotlin.common.ErrorResponse
+
+@Tag(name = "Synchronisation")
+@RestController
+@RequestMapping(value = ["sync"], produces = [MediaType.APPLICATION_JSON_VALUE])
+@AuthApiResponses
+class OfficialVisitorSyncController(private val syncFacade: SyncFacade) {
+
+  @PostMapping(path = ["/official-visit/{officialVisitId}/visitor"], produces = [MediaType.APPLICATION_JSON_VALUE])
+  @ResponseBody
+  @Operation(
+    summary = "Creates a visitor on an existing official visit",
+    description = """
+      Requires role: OFFICIAL_VISITS_MIGRATION.
+      Used to add a visitor to an official visit in DPS as part of the synchronisation from NOMIS.
+      If the contactId or offenderVisitVisitorId already exists on the visit this request will be rejected.
+      """,
+  )
+  @ApiResponses(
+    value = [
+      ApiResponse(
+        responseCode = "200",
+        description = "Successfully added the visitor",
+        content = [
+          Content(
+            mediaType = "application/json",
+            schema = Schema(implementation = SyncOfficialVisitor::class),
+          ),
+        ],
+      ),
+      ApiResponse(
+        responseCode = "404",
+        description = "The the official visit was not found using the ID presented",
+        content = [Content(schema = Schema(implementation = ErrorResponse::class))],
+      ),
+      ApiResponse(
+        responseCode = "409",
+        description = "The visitor with the IDs presented already exists on this visit",
+        content = [Content(schema = Schema(implementation = ErrorResponse::class))],
+      ),
+    ],
+  )
+  @PreAuthorize("hasAnyRole('OFFICIAL_VISITS_MIGRATION')")
+  fun syncCreateOfficialVisitor(
+    @Valid @RequestBody request: SyncCreateOfficialVisitorRequest,
+    @PathVariable(required = true) officialVisitId: Long,
+  ): SyncOfficialVisitor = syncFacade.createOfficialVisitor(officialVisitId, request)
+
+  @DeleteMapping("/official-visit/{officialVisitId}/visitor/{officialVisitorId}")
+  @Operation(
+    summary = "Delete an official visitor from a visit",
+    description = """
+      Delete an official visitor from a visit. 
+      This endpoint is idempotent, so if the visit or visitor is not present it will silently succeed.
+      """,
+  )
+  @ApiResponses(
+    value = [
+      ApiResponse(
+        responseCode = "204",
+        description = "Deleted the official visitor",
+      ),
+    ],
+  )
+  @PreAuthorize("hasAnyRole('OFFICIAL_VISITS_MIGRATION')")
+  @ResponseStatus(value = HttpStatus.NO_CONTENT)
+  fun syncDeleteOfficialVisitor(
+    @Parameter(description = "The official visit ID", required = true)
+    @PathVariable(required = true)
+    officialVisitId: Long,
+    @Parameter(description = "The official visitor ID to remove", required = true)
+    @PathVariable(required = true)
+    officialVisitorId: Long,
+  ) = syncFacade.deleteOfficialVisitor(officialVisitId, officialVisitorId)
+
+  @PutMapping(path = ["/official-visit/{officialVisitId}/visitor/{officialVisitorId}"], produces = [MediaType.APPLICATION_JSON_VALUE])
+  @ResponseBody
+  @Operation(
+    summary = "Updates a visitor on an existing official visit",
+    description = """
+      Requires role: OFFICIAL_VISITS_MIGRATION.
+      Used to update a visitor on an official visit in DPS as part of the synchronisation from NOMIS.      
+      """,
+  )
+  @ApiResponses(
+    value = [
+      ApiResponse(
+        responseCode = "200",
+        description = "Successfully updated the visitor",
+        content = [
+          Content(
+            mediaType = "application/json",
+            schema = Schema(implementation = SyncOfficialVisitor::class),
+          ),
+        ],
+      ),
+      ApiResponse(
+        responseCode = "404",
+        description = "The the official visit or visitor was not found using the IDs presented",
+        content = [Content(schema = Schema(implementation = ErrorResponse::class))],
+      ),
+    ],
+  )
+  @PreAuthorize("hasAnyRole('OFFICIAL_VISITS_MIGRATION')")
+  fun syncUpdateOfficialVisitor(
+    @Valid @RequestBody request: SyncUpdateOfficialVisitorRequest,
+    @PathVariable(required = true) officialVisitId: Long,
+    @PathVariable(required = true) officialVisitorId: Long,
+  ): SyncOfficialVisitor = syncFacade.updateOfficialVisitor(officialVisitId, officialVisitorId, request)
+}
