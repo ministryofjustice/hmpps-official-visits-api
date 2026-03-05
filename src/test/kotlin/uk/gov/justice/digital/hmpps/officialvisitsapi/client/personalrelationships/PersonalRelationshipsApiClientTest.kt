62c18dc480426144d4795388e11cdf35ff9d62d9
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2026-03-05 15:10:38 +0000

    OV-422 add new all contacts endpoint which is to replace the existing approved contacts endppoint in time.

--- src/test/kotlin/uk/gov/justice/digital/hmpps/officialvisitsapi/client/personalrelationships/PersonalRelationshipsApiClientTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/officialvisitsapi/client/personalrelationships/PersonalRelationshipsApiClientTest.kt
@@ -1,10 +1,13 @@
 package uk.gov.justice.digital.hmpps.officialvisitsapi.client.personalrelationships
 
 import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.springframework.web.reactive.function.client.WebClient
 import uk.gov.justice.digital.hmpps.officialvisitsapi.client.personalrelationships.model.ReferenceCodeGroup
+import uk.gov.justice.digital.hmpps.officialvisitsapi.helper.hasSize
 import uk.gov.justice.digital.hmpps.officialvisitsapi.helper.isEqualTo
+import uk.gov.justice.digital.hmpps.officialvisitsapi.helper.prisonerContact
 import uk.gov.justice.digital.hmpps.officialvisitsapi.integration.wiremock.PersonalRelationshipsApiMockServer
 
 class PersonalRelationshipsApiClientTest {
@@ -34,6 +37,52 @@ class PersonalRelationshipsApiClientTest {
     output.size isEqualTo 1
     output.single().isRelationshipActive isEqualTo true
     output.single().relationshipToPrisonerCode isEqualTo "POL"
+  }
+
+  @Nested
+  inner class AllPrisonerContacts {
+    private val allContacts = listOf(
+      prisonerContact("A1234BC", type = "O", currentTerm = true),
+      prisonerContact("A1234BC", type = "O", currentTerm = false),
+      prisonerContact("A1234BC", type = "S", currentTerm = true),
+      prisonerContact("A1234BC", type = "S", currentTerm = false),
+    )
+
+    @Test
+    fun `should get all prisoner contacts for prisoner number`() {
+      server.stubAllContacts(prisonerNumber = "A1234BC", prisonerContacts = allContacts)
+
+      with(client.getAllPrisonerContacts(prisonerNumber = "A1234BC")) {
+        size isEqualTo 4
+      }
+    }
+
+    @Test
+    fun `should get current term prisoner contacts only for prisoner number`() {
+      server.stubAllContacts(prisonerNumber = "A1234BC", prisonerContacts = allContacts)
+
+      val currentTermContacts = client.getAllPrisonerContacts(prisonerNumber = "A1234BC", currentTerm = true)
+
+      currentTermContacts.size isEqualTo 2
+      currentTermContacts.map { it.currentTerm } isEqualTo listOf(true, true)
+    }
+
+    @Test
+    fun `should get non-current term prisoner contacts only for prisoner number`() {
+      server.stubAllContacts(prisonerNumber = "A1234BC", prisonerContacts = allContacts)
+
+      val currentTermContacts = client.getAllPrisonerContacts(prisonerNumber = "A1234BC", currentTerm = false)
+
+      currentTermContacts.size isEqualTo 2
+      currentTermContacts.map { it.currentTerm } isEqualTo listOf(false, false)
+    }
+
+    @Test
+    fun `should be no prisoner contacts for prisoner number`() {
+      server.stubAllContacts(prisonerNumber = "A1234BC", prisonerContacts = emptyList())
+
+      client.getAllPrisonerContacts(prisonerNumber = "A1234BC", currentTerm = false) hasSize 0
+    }
   }
 
   @AfterEach
