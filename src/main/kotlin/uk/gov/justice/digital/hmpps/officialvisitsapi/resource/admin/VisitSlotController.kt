e4ac1b9fb12a278bee2cc6db6c28e99bc55e37a3
prabash.balasuriya <prabash.balasuriya@digital.justice.gov.uk>
2026-03-09 15:58:35 +0000

    OV-47 - add get by id for visit slot

--- src/main/kotlin/uk/gov/justice/digital/hmpps/officialvisitsapi/resource/admin/VisitSlotController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/officialvisitsapi/resource/admin/VisitSlotController.kt
@@ -13,6 +13,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
@@ -33,6 +34,30 @@ import uk.gov.justice.hmpps.kotlin.common.ErrorResponse
 @RequestMapping(value = ["/admin"], produces = [MediaType.APPLICATION_JSON_VALUE])
 @AuthApiResponses
 class VisitSlotController(private val facade: VisitSlotFacade) {
+
+  @GetMapping("/visit-slot/id/{visitSlotId}")
+  @Operation(
+    summary = "Get a prison visit slot by ID",
+    description = """
+          Requires role: ROLE_OFFICIAL_VISITS_ADMIN.
+          Gets the details for a prison visit slot by the visit slot's internal ID.
+          """,
+  )
+  @ApiResponses(
+    value = [
+      ApiResponse(
+        responseCode = "200",
+        description = "Successfully retrieved the prison visit slot",
+        content = [Content(mediaType = "application/json", schema = Schema(implementation = VisitSlot::class))],
+      ),
+      ApiResponse(responseCode = "404", description = "No prison visit slot with this ID was found"),
+    ],
+  )
+  @PreAuthorize("hasAnyRole('ROLE_OFFICIAL_VISITS_ADMIN')")
+  fun getVisitSlot(
+    @Parameter(description = "The internal ID for the prison visit slot", required = true)
+    @PathVariable visitSlotId: Long,
+  ): VisitSlot = facade.getVisitSlot(visitSlotId)
 
   @PostMapping(path = ["/time-slot/{prisonTimeSlotId}/visit-slot"], produces = [MediaType.APPLICATION_JSON_VALUE])
   @Operation(
