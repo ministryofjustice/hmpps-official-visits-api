1abaab29290888c5445f9156727f0d884b51a3c8
sankhajuria <sankhajuria@gmail.com>
2026-03-30 09:13:43 +0100

    OV-480 - Fix metrics value for official visit (#156)
    * OV-480 - Fix metrics value for official visit
    
    * OV-480 - Unit test added
    
    * OV-480 - Unit test added
    
    * OV-480 - Unit test added
    
    * OV-480 - Unit test added

--- src/main/kotlin/uk/gov/justice/digital/hmpps/officialvisitsapi/service/metrics/MetricsEvents.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/officialvisitsapi/service/metrics/MetricsEvents.kt
@@ -110,23 +110,25 @@ data class OfficialVisitMetricTelemetry(
   }
 
   fun VisitMetricInfo.hoursBeforeStartTimeMetric(): Pair<String, Double> = "hoursBeforeStartTime" to ChronoUnit.HOURS.between(
-    startTime,
-    LocalTime.now(),
+    LocalTime.now().withSecond(0).withNano(0),
+    startTime?.withSecond(0)?.withNano(0),
   ).toDouble()
 
-  fun VisitMetricInfo.hoursAfterStartTimeTimeMetrics(): Pair<String, Double> = "hoursAfterStartTime" to ChronoUnit.HOURS.between(LocalTime.now(), startTime).toDouble()
+  fun VisitMetricInfo.hoursAfterStartTimeTimeMetrics(): Pair<String, Double> = "hoursAfterStartTime" to
+    ChronoUnit.HOURS.between(startTime?.withSecond(0)?.withNano(0), LocalTime.now().withSecond(0).withNano(0)).toDouble()
 
   fun VisitMetricInfo.numberOfVisitors(): Pair<String, Double> = "number_of_visitors" to numberOfVisitors.toDouble()
 }
 
 private fun VisitMetricInfo.visitAdditionalInfo(): Map<String, String> = mapOf(
   "prisoner_number" to prisonerNumber,
+  "official_visit_id" to "$officialVisitId",
 )
 
 private fun VisitorMetricInfo.visitorAdditionalInfo(): Map<String, String> = mapOf(
-  "official_visit_id" to "$officialVisitId",
   "official_visitor_id" to "$officialVisitorId",
   "contact_id" to "$contactId",
+  "official_visit_id" to "$officialVisitId",
 )
 
 private fun TimeSlotInfo.timeSLotInfo(): Map<String, String> = mapOf(
@@ -139,7 +141,7 @@ private fun OfficialVisitMetricTelemetry.visitMetrics(
     .takeIf { eventType == MetricsEvents.CREATE.eventType || eventType == MetricsEvents.AMEND.eventType || eventType == MetricsEvents.CANCEL.eventType },
   additionalInformation.numberOfVisitors().takeIf { eventType == MetricsEvents.CREATE.eventType },
   additionalInformation.hoursAfterStartTimeTimeMetrics()
-    .takeIf { eventType == MetricsEvents.CREATE.eventType },
+    .takeIf { eventType == MetricsEvents.COMPLETE.eventType },
 ).toMap()
 
 private fun SearchInfo.searchAdditionalInfo(): Map<String, String> = mapOf(
@@ -147,7 +149,7 @@ private fun SearchInfo.searchAdditionalInfo(): Map<String, String> = mapOf(
   "end_date" to "$endDate",
   "search_term" to searchTerm.orEmpty(),
   "visit_types" to "$visitTypes",
-  "visit_statuses" to "visitStatuses",
+  "visit_statuses" to "$visitStatuses",
   "location_Ids" to "$locationIds",
 )
 
