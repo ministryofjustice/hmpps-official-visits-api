3c3684202455715b7cf80b96a3770aba303ce082
Prabash Balasuriya <prabash.balasuriya@justice.gov.uk>
2026-04-15 09:12:46 +0100

    OV-507: advertise active prisons (#171)
    * OV-507: advertise active prisons
    
    * OV-507: advertise active prisons
    
    ---------
    
    Co-authored-by: prabash.balasuriya <prabash.balasuriya@digital.justice.gov.uk>

--- src/main/kotlin/uk/gov/justice/digital/hmpps/officialvisitsapi/OfficialVisitsApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/officialvisitsapi/OfficialVisitsApi.kt
@@ -1,8 +1,11 @@
 package uk.gov.justice.digital.hmpps.officialvisitsapi
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import uk.gov.justice.digital.hmpps.officialvisitsapi.config.ServiceConfig
 
+@EnableConfigurationProperties(ServiceConfig::class)
 @SpringBootApplication
 class OfficialVisitsApi
 
