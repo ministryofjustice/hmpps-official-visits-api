1e794c3080a1ed5fc3323e602dc4e28c89aa614d
prabash.balasuriya <prabash.balasuriya@justice.gov.uk>
2026-05-13 11:12:56 +0100

    OV-603: Fix video visit capacity handling and improve integration tests

--- src/test/kotlin/uk/gov/justice/digital/hmpps/officialvisitsapi/helper/Prisons.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/officialvisitsapi/helper/Prisons.kt
@@ -7,7 +7,6 @@ import java.time.DayOfWeek
 import java.time.DayOfWeek.FRIDAY
 import java.time.LocalDate
 import java.time.LocalTime
-import java.util.UUID
 
 const val BIRMINGHAM = "BMI"
 const val WANDSWORTH = "WWI"
