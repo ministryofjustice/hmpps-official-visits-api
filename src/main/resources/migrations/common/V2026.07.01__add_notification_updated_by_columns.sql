b1bde7914c614fb85cc88ef4daa367b91405cf1b
prabash_moj <prabash.balasuriya@justice.gov.uk>
2026-07-02 13:06:33 +0100

    OV-779 : record created by value in the notification table

--- src/main/resources/migrations/common/V2026.07.01__add_notification_updated_by_columns.sql
+++ src/main/resources/migrations/common/V2026.07.01__add_notification_updated_by_columns.sql
@@ -4,7 +4,3 @@ ALTER TABLE notification
 
 -- Indexes to support email search by created_by
 CREATE INDEX IF NOT EXISTS idx_notification_created_by ON notification(created_by);
-
--- Indexes to support email search by visit id and created_by
-CREATE INDEX IF NOT EXISTS idx_notification_visit_id_created_by
-    ON notification (official_visit_id, created_by DESC);
