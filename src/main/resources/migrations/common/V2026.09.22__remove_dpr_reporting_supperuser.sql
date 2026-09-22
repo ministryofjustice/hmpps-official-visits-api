de1c19a2d5908202d854b26a559d2fe619c7c895
prabash_moj <prabash.balasuriya@justice.gov.uk>
2026-09-22 14:19:20 +0100

    OV-914 : revoke rds_superuser after dpr ingestion setup is complete

--- src/main/resources/migrations/common/V2026.09.22__remove_dpr_reporting_supperuser.sql
+++ src/main/resources/migrations/common/V2026.09.22__remove_dpr_reporting_supperuser.sql
@@ -1,2 +1,8 @@
 --remove rds_superuser from digital_prison_reporting
-REVOKE rds_superuser from digital_prison_reporting;
+DO $$
+BEGIN
+    IF EXISTS (SELECT 1 FROM pg_available_extensions WHERE name = 'pglogical') THEN
+        REVOKE rds_superuser from digital_prison_reporting; --GRANT rds_superuser to digital_prison_reporting;
+    END IF;
+END
+$$;
