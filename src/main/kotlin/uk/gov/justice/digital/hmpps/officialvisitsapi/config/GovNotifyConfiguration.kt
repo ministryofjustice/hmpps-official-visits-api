1f2210091288e04bb1b17afe5478a173ebf9dba5
Matt Crowson <60104344+matt-crowson-moj@users.noreply.github.com>
2026-09-24 17:35:27 +0100

    OV-806 adding in new email template identifiers in prepration for new emails. (#313)
    * OV-806 adding in new email template identifiers in prepration for new emails.
    
    * OV-806 address minor PR comment.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/officialvisitsapi/config/GovNotifyConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/officialvisitsapi/config/GovNotifyConfiguration.kt
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration
 import uk.gov.justice.digital.hmpps.officialvisitsapi.service.notifications.EmailService
 import uk.gov.justice.digital.hmpps.officialvisitsapi.service.notifications.EmailTemplate
 import uk.gov.justice.digital.hmpps.officialvisitsapi.service.notifications.EmailTemplates
+import uk.gov.justice.digital.hmpps.officialvisitsapi.service.notifications.EmailType
 import uk.gov.justice.digital.hmpps.officialvisitsapi.service.notifications.EmailType.OFFICIAL_VISIT_CANCELLED
 import uk.gov.justice.digital.hmpps.officialvisitsapi.service.notifications.EmailType.OFFICIAL_VISIT_CREATED
 import uk.gov.justice.digital.hmpps.officialvisitsapi.service.notifications.EmailType.OFFICIAL_VISIT_UPDATED
@@ -20,6 +21,15 @@ class GovNotifyConfiguration(
   @Value($$"${notify.templates.official-visit-created:}") private val officialVisitCreatedTemplateId: String,
   @Value($$"${notify.templates.official-visit-cancelled:}") private val officialVisitCancelledTemplateId: String,
   @Value($$"${notify.templates.official-visit-updated:}") private val officialVisitUpdatedTemplateId: String,
+  @Value($$"${notify.templates.in-person-visit.confirmed:}") private val inPersonVisitConfirmedTemplateId: String,
+  @Value($$"${notify.templates.in-person-visit.amended:}") private val inPersonVisitAmendedTemplateId: String,
+  @Value($$"${notify.templates.in-person-visit.cancelled:}") private val inPersonVisitCancelledTemplateId: String,
+  @Value($$"${notify.templates.telephone-visit.confirmed:}") private val telephoneVisitConfirmedTemplateId: String,
+  @Value($$"${notify.templates.telephone-visit.amended:}") private val telephoneVisitAmendedTemplateId: String,
+  @Value($$"${notify.templates.telephone-visit.cancelled:}") private val telephoneVisitCancelledTemplateId: String,
+  @Value($$"${notify.templates.video-visit.confirmed:}") private val videoVisitConfirmedTemplateId: String,
+  @Value($$"${notify.templates.video-visit.amended:}") private val videoVisitAmendedTemplateId: String,
+  @Value($$"${notify.templates.video-visit.cancelled:}") private val videoVisitCancelledTemplateId: String,
 ) {
   companion object {
     private val logger = LoggerFactory.getLogger(this::class.java)
@@ -39,13 +49,19 @@ class GovNotifyConfiguration(
 
   @Bean
   fun emailTemplates() = EmailTemplates(
-    setOfNotNull(
-      officialVisitCreatedTemplateId.takeIf { it.isNotBlank() }
-        ?.let { EmailTemplate(it, OFFICIAL_VISIT_CREATED) },
-      officialVisitCancelledTemplateId.takeIf { it.isNotBlank() }
-        ?.let { EmailTemplate(it, OFFICIAL_VISIT_CANCELLED) },
-      officialVisitUpdatedTemplateId.takeIf { it.isNotBlank() }
-        ?.let { EmailTemplate(it, OFFICIAL_VISIT_UPDATED) },
+    setOf(
+      EmailTemplate(officialVisitCreatedTemplateId, OFFICIAL_VISIT_CREATED),
+      EmailTemplate(officialVisitCancelledTemplateId, OFFICIAL_VISIT_CANCELLED),
+      EmailTemplate(officialVisitUpdatedTemplateId, OFFICIAL_VISIT_UPDATED),
+      EmailTemplate(inPersonVisitConfirmedTemplateId, EmailType.IN_PERSON_VISIT_CONFIRMED),
+      EmailTemplate(inPersonVisitAmendedTemplateId, EmailType.IN_PERSON_VISIT_AMENDED),
+      EmailTemplate(inPersonVisitCancelledTemplateId, EmailType.IN_PERSON_VISIT_CANCELLED),
+      EmailTemplate(telephoneVisitConfirmedTemplateId, EmailType.TELEPHONE_VISIT_CONFIRMED),
+      EmailTemplate(telephoneVisitAmendedTemplateId, EmailType.TELEPHONE_VISIT_AMENDED),
+      EmailTemplate(telephoneVisitCancelledTemplateId, EmailType.TELEPHONE_VISIT_CANCELLED),
+      EmailTemplate(videoVisitConfirmedTemplateId, EmailType.VIDEO_VISIT_CONFIRMED),
+      EmailTemplate(videoVisitAmendedTemplateId, EmailType.VIDEO_VISIT_AMENDED),
+      EmailTemplate(videoVisitCancelledTemplateId, EmailType.VIDEO_VISIT_CANCELLED),
     ),
   )
 }
--- src/test/kotlin/uk/gov/justice/digital/hmpps/officialvisitsapi/config/GovNotifyConfigurationTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/officialvisitsapi/config/GovNotifyConfigurationTest.kt
@@ -12,6 +12,15 @@ class GovNotifyConfigurationTest {
       officialVisitCreatedTemplateId = "created-template-id",
       officialVisitCancelledTemplateId = "cancelled-template-id",
       officialVisitUpdatedTemplateId = "updated-template-id",
+      inPersonVisitConfirmedTemplateId = "in-person-visit-confirmed-template-id",
+      inPersonVisitAmendedTemplateId = "in-person-visit-amended-template-id",
+      inPersonVisitCancelledTemplateId = "in-person-visit-cancelled-template-id",
+      telephoneVisitConfirmedTemplateId = "telephone-visit-confirmed-template-id",
+      telephoneVisitAmendedTemplateId = "telephone-visit-amended-template-id",
+      telephoneVisitCancelledTemplateId = "telephone-visit-cancelled-template-id",
+      videoVisitConfirmedTemplateId = "video-visit-confirmed-template-id",
+      videoVisitAmendedTemplateId = "video-visit-amended-template-id",
+      videoVisitCancelledTemplateId = "video-visit-cancelled-template-id",
     )
 
     val templates = configuration.emailTemplates()
@@ -19,5 +28,14 @@ class GovNotifyConfigurationTest {
     templates.templateIdFor(EmailType.OFFICIAL_VISIT_CREATED) isEqualTo "created-template-id"
     templates.templateIdFor(EmailType.OFFICIAL_VISIT_CANCELLED) isEqualTo "cancelled-template-id"
     templates.templateIdFor(EmailType.OFFICIAL_VISIT_UPDATED) isEqualTo "updated-template-id"
+    templates.templateIdFor(EmailType.IN_PERSON_VISIT_CONFIRMED) isEqualTo "in-person-visit-confirmed-template-id"
+    templates.templateIdFor(EmailType.IN_PERSON_VISIT_AMENDED) isEqualTo "in-person-visit-amended-template-id"
+    templates.templateIdFor(EmailType.IN_PERSON_VISIT_CANCELLED) isEqualTo "in-person-visit-cancelled-template-id"
+    templates.templateIdFor(EmailType.TELEPHONE_VISIT_CONFIRMED) isEqualTo "telephone-visit-confirmed-template-id"
+    templates.templateIdFor(EmailType.TELEPHONE_VISIT_AMENDED) isEqualTo "telephone-visit-amended-template-id"
+    templates.templateIdFor(EmailType.TELEPHONE_VISIT_CANCELLED) isEqualTo "telephone-visit-cancelled-template-id"
+    templates.templateIdFor(EmailType.VIDEO_VISIT_CONFIRMED) isEqualTo "video-visit-confirmed-template-id"
+    templates.templateIdFor(EmailType.VIDEO_VISIT_AMENDED) isEqualTo "video-visit-amended-template-id"
+    templates.templateIdFor(EmailType.VIDEO_VISIT_CANCELLED) isEqualTo "video-visit-cancelled-template-id"
   }
 }
